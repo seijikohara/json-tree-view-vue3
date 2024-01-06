d55b1bb7dbebc1f8255cf3812c9c9a621b6abb52
Seiji Kohara <sage.0118@gmail.com>
2024-01-07 07:34:50 +0900

    Configured TypeScript compilation to JavaScript

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,36 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import dts from 'vite-plugin-dts'
+
+const name = 'json-tree-view-vue3'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
+    dts(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  build: {
+    lib: {
+      entry: 'src/components/index.ts',
+      name,
+      fileName: (format) => `${name}.${format}.js`
+    },
+    sourcemap: true,
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  },
 })
